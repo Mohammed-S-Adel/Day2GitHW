59a390f048ecd628e13e57944afe4015a7ae8fb1
moham <moham@Mohammed.attlocal.net>
2023-07-29 17:20:11 -0700

    Git Day2 HW

---
+++ src/homework/day2/DemoHW.java
@@ -0,0 +1,19 @@
+package homework.day2;
+
+public class DemoHW {
+	
+	public static void main(String[] args) {
+		System.out.println("MOhammed");
+		
+		String country = "USA";
+		String state = "CA" ;
+		
+		
+	}
+	
+	public static void add(int a , int b) {
+		System.out.println(a + b);
+	}
+
+
+}
